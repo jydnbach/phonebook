eab7c0293f078cb6c35243a35d4a92a045c68f6b
jydnbach <jaydenkim95@gmail.com>
2025-03-07 15:15:18 +0700

    update info at /info

--- index.js
+++ index.js
@@ -32,6 +32,13 @@ app.get('/api/persons', (req, res) => {
   res.json(persons);
 });
 
+app.get('/info', (req, res) => {
+  res.send(`
+    Phonebook has info for ${persons.length} people
+    ${new Date().toDateString()} ${new Date().toLocaleTimeString()}
+    `);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
