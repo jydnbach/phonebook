830f6d82cb91ff7545458105faeaae671f5997e9
jydnbach <jaydenkim95@gmail.com>
2025-03-12 09:31:21 +0700

    mongoose add num to phoenbook setup

---
+++ mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const password = process.argv[2];
+const url = `mongodb+srv://jydndev:${password}@cluster0.t2jkd.mongodb.net/phoneBook?retryWrites=true&w=majority&appName=Cluster0`;
+
+if (process.argv.length < 3) {
+  console.log('give password as argument');
+  process.exit(1);
+}
+
+mongoose.set('strictQuery', false);
+
+mongoose.connect(url);
+console.log('connected');
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+if (process.argv.length === 5) {
+  const name = process.argv[3];
+  const number = process.argv[4];
+
+  const newPerson = new Person({ name, number });
+
+  newPerson.save().then(() => {
+    console.log(`added ${name} ${number} to phonebook`);
+    mongoose.connection.close();
+  });
+}
+
+if (process.argv.length === 3) {
+  Person.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(person);
+    });
+
+    mongoose.connection.close();
+  });
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+web service address: https://phonebook-80ti.onrender.com/
